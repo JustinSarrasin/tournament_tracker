c2c1334d373eb5ce689a2b2242cba5518152fb59
Justin Sarrasin <sarrasin.justin@gmail.com>
2018-02-22 14:45:45 -0500

    initial commit

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
     render() {
       return (
         <div>
